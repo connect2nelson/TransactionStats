0ad3a1095ea86bcef941a06ec2c7a834ce6aa5e1
nelsondavenapalli <nelson.csits@gmail.com>
2018-03-23 21:57:29 +0100

    Initial Commit - Creating the project structure.

---
+++ src/main/java/com/n26/stats/TransactionStatistics/TransactionStatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.n26.stats.TransactionStatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionStatisticsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/n26/stats/TransactionStatistics/TransactionStatisticsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.n26.stats.TransactionStatistics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransactionStatisticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
